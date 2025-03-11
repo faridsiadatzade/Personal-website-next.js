333014c8f691f494868aa25e4c4a7fed779d64b6
fsiadatzade <fsiadatzade@gmail.com>
2025-03-11 16:22:06 +0330

    [FIX]: add new config (next.config)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://raw.githubusercontent.com/faridsiadatzade/faridsiadatzade.github.io/refs/heads/master/docs/' : '',
+  // assetPrefix: '',
   images: {
     unoptimized: true,
   },
