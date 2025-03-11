f3133573a3e23f67e1ee701239c194a07eece9ef
fsiadatzade <fsiadatzade@gmail.com>
2025-03-11 15:51:16 +0330

    [FIX]: check new config (next.config)

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://faridsi.ir' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://faridsi.ir/docs' : '',
   images: {
     unoptimized: true,
   },
